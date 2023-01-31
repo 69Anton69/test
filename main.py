654b9bb2ee65826f800f8b924f4525d64e0da888
Your Name <you@example.com>
2023-01-31 12:43:36 +0200

    Commited

--- main.py
+++ main.py
@@ -3,4 +3,16 @@ import random
 a_1 = random.randint(1, 6)
 b_1 = random.randint(1, 6)
 
-print(a_1, b_1)
+user_1 = a_1 + b_1
+ 
+a_2 = random.randint(1, 6)
+b_2 = random.randint(1, 6)
+
+user_2 = a_2 + b_2
+
+if user_1 > user_2:
+    print("winner player 1")
+elif user_1 < user_2:
+    print("winner player 2")
+else:
+    print("There is a tie between you") 
